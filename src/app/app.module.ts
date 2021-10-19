fae40f6dc9d2388b3fcf4c6edaa1365317ecad76
tercette <MY_NAME@example.comleandro.tercette@gmail.com>
2021-10-18 23:12:45 -0300

    tomanocuporra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BankAccountComponent } from './bank-account/bank-account.component';
 
-
+//vai toma no cu caraio
 @NgModule({
   declarations: [
     AppComponent,
